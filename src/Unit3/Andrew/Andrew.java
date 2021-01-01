044992a7283c52faca3e1af6902a0569117e6c88
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2020-12-31 22:32:24 -0800

    impleamtion

---
+++ src/Unit3/Andrew/Andrew.java
@@ -0,0 +1,68 @@
+package Unit3.Andrew;
+
+public class Andrew {
+}
+
+/** Question #1 **/
+/** SHOW ALL YOUR WORK. REMEMBER THAT PROGRAM SEGMENTS ARE TO BE WRITTEN IN JAVA.
+
+ Assume that the classes listed in the Java Quick Reference have been imported where appropriate.
+ Unless otherwise noted in the question, assume that parameters in method calls are not null and that methods are called only when their preconditions are satisfied.
+ In writing solutions for each question, you may use any of the accessible methods that are listed in classes defined in that question.
+ Writing significant amounts of code that can be replaced by a call to one of these methods will not receive full credit.
+
+ Assume that the following variables have been properly declared and initialized.
+
+ a boolean variable named rsvp
+ an int variable named selection, where 1 represents "beef", 2 represents "chicken", 3 represents "pasta", and all other values represent "fish"
+ a String variable named option1
+ a String variable named option2 **/
+
+class Party
+{
+    Party()
+    {
+        String option1;
+        Boolean rsvp;
+        String option2;
+    }
+
+}
+
+
+
+
+
+/** Question #2 **/
+/** SHOW ALL YOUR WORK. REMEMBER THAT PROGRAM SEGMENTS ARE TO BE WRITTEN IN JAVA.
+ Assume that the classes listed in the Java Quick Reference have been imported where appropriate.
+ Unless otherwise noted in the question, assume that parameters in method calls are not null and that methods are called only when their preconditions are satisfied.
+ In writing solutions for each question, you may use any of the accessible methods that are listed in classes defined in that question.
+ Writing significant amounts of code that can be replaced by a call to one of these methods will not receive full credit.
+ This question involves the Draw class, which is used to draw line segments and squares on a  10 -by- 10   xy -coordinate grid. **/
+class Draw
+{
+
+    /** Precondition: All parameters are between 0 and 10, inclusive.
+     * Draws a line segment in a  10 -by- 10   xy -coordinate grid.
+     * The line segment is drawn from (x1, y1) to (x2, y2).
+     */
+    public void drawLine(int x1, int y1, int x2, int y2)
+    { /* implementation not shown */ }
+
+
+    /** Precondition: 0 ≤ x < 10, 0 < y ≤ 10, and len > 0.
+     * Draws a square on a  10 -by- 10   xy -coordinate grid
+     * and prints the square’s side length and area.
+     * The upper left corner of the square will be located
+     * at the coordinate (x, y) and the side length of the
+     * square will be len (or as large as will fit in the grid).
+     */
+    public void drawSquare(int x, int y, int len) {
+        drawLine(x, y, x, y-len);
+        drawLine(x, y-len, x+len, y-len);
+        drawLine(x+len, y-len, x+len, y);
+        drawLine(x+len, y, x, y);
+    }
+
+}
