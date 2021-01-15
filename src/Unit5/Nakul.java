bfaaef3e235e8a5c0454dda68b86587e01791773
Nakul Nandhakumar <nakul@nandhakumar.net>
2021-01-14 20:10:24 -0800

    Comments for Unit 5 Question 1 added

--- src/Unit5/Nakul.java
+++ src/Unit5/Nakul.java
@@ -16,13 +16,13 @@ public class Nakul {
      }
 
      /** (a) Write a method for the Invitation class that returns the name of the host. Write the method below. **/
-     public String getHostName() {
+     public String getHostName() {  // Simple get method that returns the hostName. IntelliJ does this automatically for you.
          return hostName;
      }
 
      /** (b) Write a method for the Invitation class that accepts a parameter and uses it to update the address for the event.
             Write the method below. **/
-     public void updateAddress(String newAddress){
+     public void updateAddress(String newAddress){ // Updating the instance variable address by passing a newAddress into the method
          address = newAddress;
      }
 
@@ -30,7 +30,7 @@ public class Nakul {
       For example, if the host name is "Karen", the party location is "1234 Walnut Street", and the person invited is "Cheryl", the method should return a string in the following format.
       Dear Cheryl, please attend my event at 1234 Walnut Street. See you then, Karen.
       Write the method below. Your implementation must conform to the example above. **/
-     public void sendInvitation(String invitedGuestName){
+     public void sendInvitation(String invitedGuestName){  // Print statement which substitutes out the guest, host, and address with their respective variables
          System.out.println("Dear "+invitedGuestName+", please attend my event at "+address+". See you then, "+hostName);
      }
 
@@ -46,12 +46,12 @@ public class Nakul {
       Write a correct implementation of the one-parameter Invitation constructor that avoids the error in the student’s implementation.
       Write the method below. **/
 
-     public Invitation(String address) {
+     public Invitation(String address) { // The student didn't differentiate between instance and local variable and the constructor thought they were both local variables. You need the this.address.
          this.address = address;
          hostName = "Host";
      }
 
-     public static void main(String[] args) {
+     public static void main(String[] args) {  // Simple implementation and object creation to test if code works.
          Invitation invitation = new Invitation("Nakul", "12143 Deer Ridge Avenue");
          System.out.println(invitation.getHostName());
          invitation.updateAddress("52956 Cherry Blossom Court");
