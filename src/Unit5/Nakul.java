3565deabcb77fc5e641eeeaba1c7e4e72afadae0
Nakul Nandhakumar <nakul@nandhakumar.net>
2021-01-16 23:06:31 -0800

    Added Unit 6 Package and Andrew and Nakul class files

--- src/Unit5/Nakul.java
+++ src/Unit5/Nakul.java
@@ -56,10 +56,10 @@ public class Nakul {
      }
 
      public static void main(String[] args) {  // Simple implementation and object creation to test if code works.
-         Invitation invitation = new Invitation("Nakul", "12143 Deer Ridge Avenue");
+         Invitation invitation = new Invitation("Unit6.Nakul", "12143 Deer Ridge Avenue");
          System.out.println(invitation.getHostName());
          invitation.updateAddress("52956 Cherry Blossom Court");
-         invitation.sendInvitation("Andrew");
+         invitation.sendInvitation("Unit6.Andrew");
 
          Invitation invitation2 = new Invitation("29573 Heaven Road");
          System.out.println(invitation2.getHostName());
