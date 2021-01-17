b9b9b6861ddfe3ff673ad75522e801fd36a2abac
Nakul Nandhakumar <nakul@nandhakumar.net>
2021-01-16 23:31:04 -0800

    Unit g Question #1 Done with implementation

--- src/Unit6/Nakul.java
+++ src/Unit6/Nakul.java
@@ -2,3 +2,46 @@ package Unit6;
 
 public class Nakul {
 }
+
+
+/** QUESTION #1 **/
+
+/**
+ * An array of String objects, words, has been properly declared and initialized.
+ * Each element of words contains a String consisting of lowercase letters (a–z).
+ * Write a code segment that uses an enhanced for loop to print all elements of words that end with "ing".
+ * As an example, if words contains {"ten", "fading", "post", "card", "thunder", "hinge", "trailing", "batting"}, then the following output should be produced by the code segment:
+
+ fading
+ trailing
+ batting
+
+ * Write the code segment as described above. The code segment must use an enhanced for loop to earn full credit. **/
+
+class Ing {
+    String[] words;
+    int lengthOfWord;
+
+    public Ing(String[] arr){
+        words = arr;
+    }
+
+    public void printWordsEndingInIng(){
+        for (String word: words){
+            lengthOfWord = word.length();
+
+            if (lengthOfWord < 3){
+                continue;
+            }
+            if (word.substring(lengthOfWord - 3).equals("ing")){
+                System.out.println(word);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        String[] arrOfStrings = {"a", "run", "strike", "man", "coming", "dude", "mistering", "through", "sipping", "listening"};
+        Ing ing = new Ing(arrOfStrings);
+        ing.printWordsEndingInIng();
+    }
+}
