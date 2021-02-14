3153e3a6aa1b3df0629ac8b15391e6e53dffac8f
Nakul Nandhakumar <nakul@nandhakumar.net>
2021-02-13 21:42:11 -0800

    Unit 9 Question 2 Done with comments added

--- src/Unit9/Nakul.java
+++ src/Unit9/Nakul.java
@@ -101,8 +101,90 @@ class BookListing {
 
     public void printDescription(){
         book.printBookInfo();  // Prints info about book regardless of it being a picture book or a book
-        System.out.println(", "+price); // Prints the price
+        System.out.println(", $"+price); // Prints the price
     }
 
     /** FOR IMPLEMENTATION SEE ABOVE IMPLEMENTATION CLASS **/
+}
+
+
+/** QUESTION 2 **/
+
+/** A set of classes using inheritance is used to represent animals observed at a wildlife sanctuary.
+
+All Animal objects have the following attributes:
+ * A String variable indicating whether the animal is a carnivore or a herbivore
+ * A String variable representing the animal species (e.g., lion, giraffe, zebra)
+ * A String variable representing the name of the individual animal (e.g., Lisa, Gary, Percy)
+ * The Animal class also contains a toString method that indicates the state of an animal. **/
+
+class Animal {
+    private String type;
+    private String species;
+    private String name;
+
+    public Animal(String t, String s, String n) { // Initializing all animal instance variables through constructor
+        type = t;
+        species = s;
+        name = n;
+    }
+
+    @Override
+    public String toString() {  // toString method overrides previous toString method.
+        return name+" the "+species+" is a "+type;
+    }
+
+    public static void main(String[] args) {
+        Animal lisa = new Animal("carnivore", "lion", "Lisa"); // Creating lisa the lion is a carnivore
+        System.out.println(lisa.toString()); // Correctly prints lisa the lion
+    }
+}
+
+/** The Herbivore class is a subclass of Animal.
+ * The Herbivore class does not contain any attributes or methods other than those found in the Animal class.
+ * The constructor to the Herbivore class accepts two parameters for the species and name of the herbivore.
+ * The constructor passes those parameters along with the string literal "herbivore" to the Animal class to construct the object. **/
+
+/** (b)  Write the complete Herbivore class.
+ * Your implementation must meet all specifications and conform to the behavior shown in the table. **/
+class Herbivore extends Animal{
+
+    public Herbivore(String s, String n){  // Constructor takes parameters and passes them to superclass with type
+        super("herbivore", s, n);
+    }
+
+    public static void main(String[] args) {  // Implementation shows code works exactly as it is supposed to
+        Herbivore gary = new Herbivore("giraffe", "Gary");  // Creating herbivore class object
+        System.out.println(gary.toString());  // Calling toString which calls toString method of superclass
+    }
+}
+
+/** The Elephant class is a subclass of Herbivore.
+ * The Elephant class contains one additional attribute not found in Herbivore: a double variable representing
+ * the length of the elephant’s tusks, in meters.
+ * The constructor to the Elephant class accepts two parameters for the name and tusk length of the elephant.
+ * The constructor passes those parameters along with the string literal "elephant" to the Herbivore class to
+ * construct the object. **/
+
+/** (c)   Write the complete Elephant class.
+ * Your implementation must meet all specifications and conform to the behavior shown in the table. **/
+
+class Elephant extends Herbivore{
+    private double tuskLength; // tusklength variable
+
+    public Elephant(String n, double tl){
+        super("elephant", n);  // calls superclass constructor which calls animal constructor
+        tuskLength = tl; // initializes tusklength
+    }
+
+    @Override
+    public String toString() { // Overrides the superclass toString (Animal class toString)
+        return super.toString() + " with tusks "+tuskLength+" meters long";
+        // Calls Animal class toString for initial part of return message and then returns the rest of required message
+    }
+
+    public static void main(String[] args) { // Implementation shows that code works.
+        Elephant percy = new Elephant("Percy", 2.0);
+        System.out.println(percy.toString());
+    }
 }
