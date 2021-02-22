08fdabffb96d9a6505897d236485eff60407e692
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-02-21 21:35:28 -0800

    Unit 10

--- src/Unit10/Andrew/Andrew.java
+++ src/Unit10/Andrew/Andrew.java
@@ -1,4 +1,40 @@
 package Unit10.Andrew;
 
 public class Andrew {
+
+
+}
+
+class Numbers
+{
+    public static int gcf(int a, int b)
+    {
+
+        if (a%b==0) // division is clean
+        {
+            return b;
+        }
+        else
+        {
+            return gcf(b,a%b); // wording of problem sounds like b should go first then a
+        }
+
+
+    }
+
+    public static void reduceFraction(int num, int dom)
+    {
+            int reducer = gcf(num, dom);
+            num/=reducer;
+            dom/=reducer;
+        System.out.println(num+"/"+dom);
+
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(gcf(3,7));
+        reduceFraction(3,6);
+    }
+
 }
