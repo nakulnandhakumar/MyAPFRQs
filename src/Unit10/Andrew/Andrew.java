9be84a4168a10478252d5b2267b24d75e82753a6
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-02-26 23:40:11 -0800

    added unit 9 with update packages and updated bin files

--- src/Unit10/Andrew/Andrew.java
+++ src/Unit10/Andrew/Andrew.java
@@ -27,14 +27,19 @@ class Numbers
             int reducer = gcf(num, dom);
             num/=reducer;
             dom/=reducer;
-        System.out.println(num+"/"+dom);
+            if (num%dom == 0)
+            {
+                System.out.println(num/dom);
+            }
+            else
+                System.out.println(num+"/"+dom);
 
     }
 
     public static void main(String[] args)
     {
         System.out.println(gcf(3,7));
-        reduceFraction(3,6);
+        reduceFraction(3,9);
     }
 
 }
